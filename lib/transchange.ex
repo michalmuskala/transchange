f02ad778a733df2657901a3fca71073b75d3ef1c
Michal Muskala <michal@muskala.eu>
2015-11-14 22:34:53 +0100

    Initial implementation

--- lib/transchange.ex
+++ lib/transchange.ex
@@ -1,2 +1,126 @@
 defmodule Transchange do
+  alias Ecto.Changeset
+
+  defstruct changesets: [], ok_pipeline: [], error_pipeline: []
+
+  def new do
+    %Transchange{}
+  end
+
+  def insert(transchange, name, changeset) do
+    add_changeset(transchange, :insert, name, changeset)
+  end
+
+  def update(transchange, name, changeset) do
+    add_changeset(transchange, :update, name, changeset)
+  end
+
+  def delete(transchange, name, changeset) do
+    add_changeset(transchange, :delete, name, changeset)
+  end
+
+  def pipe_ok(transchange, fun) do
+    add_pipeline(transchange, :ok_pipeline, fun)
+  end
+
+  def pipe_error(transchange, fun) do
+    add_pipeline(transchange, :error_pipeline, fun)
+  end
+
+  def run(%Transchange{} = transchange, repo, opts \\ []) when is_atom(repo) do
+    transchange.changesets
+    |> Enum.reverse
+    |> check_changesets_valid
+    |> run_transaction(repo, opts)
+    |> run_pipeline(transchange)
+  end
+
+  defp check_changesets_valid(changesets) do
+    invalid? = fn {_, changeset} -> not changeset.valid? end
+
+    case Enum.filter(changesets, invalid?) do
+      [] ->
+        {:ok, changesets}
+      invalid ->
+        {:error, invalid}
+    end
+  end
+
+  defp run_transaction({:ok, []}, _repo, _opts) do
+    {:ok, []}
+  end
+
+  defp run_transaction({:ok, changesets}, repo, opts) do
+    repo.transaction(fn ->
+      run_changesets(changesets, repo, opts, [])
+    end, opts)
+  end
+
+  defp run_transaction({:error, invalid}, _repo, _opts) do
+    {:error, invalid}
+  end
+
+  defp run_pipeline({:ok, result}, transchange) do
+    do_run_pipeline(result, transchange.ok_pipeline)
+  end
+
+  defp run_pipeline({:error, result}, transchange) do
+    do_run_pipeline(result, transchange.error_pipeline)
+  end
+
+  defp do_run_pipeline(result, pipeline) do
+    pipeline
+    |> Enum.reverse
+    |> Enum.reduce(result, fn
+      {m, f, a}, result ->
+        apply(m, f, [result | a])
+      fun, result when is_function(fun) ->
+        apply(fun, [result])
+    end)
+  end
+
+  defp run_changesets([], _repo, _opts, acc) do
+    Enum.reverse(acc)
+  end
+
+  defp run_changesets([{name, changeset} | rest], repo, opts, acc) do
+    case apply(repo, changeset.action, [changeset, opts]) do
+      {:ok, model} ->
+        run_changesets(rest, repo, opts, [model | acc])
+      {:error, changeset} ->
+        repo.rollback([{name, changeset}])
+    end
+  end
+
+  defp add_changeset(%Transchange{} = transchange, action, name, changeset) do
+    element = {name, put_action(changeset, action)}
+    update_in transchange.changesets, &[element | &1]
+  end
+
+  defp add_pipeline(%Transchange{} = transchange, name, fun)
+      when is_function(fun, 1) or tuple_size(fun) == 3 do
+    Map.update!(transchange, name, &[fun | &1])
+  end
+
+  defp put_action(%Changeset{action: nil} = changeset, action) do
+    %{changeset | action: action}
+  end
+
+  defp put_action(%Changeset{action: action} = changeset, action) do
+    changeset
+  end
+
+  defp put_action(%Changeset{action: original}, action) do
+    raise ArgumentError, "you provided a changeset with an already set action " <>
+      "of #{original} when trying to #{action} it"
+  end
+
+  defp put_action(_changeset, _action) do
+    raise ArgumentError, "Transchange operates only on changesets"
+  end
+
+  @doc false
+  def reverse_apply(arg, fun) do
+    fun.(arg)
+  end
 end
--- test/transchange_test.exs
+++ test/transchange_test.exs
@@ -2,7 +2,121 @@ defmodule TranschangeTest do
   use ExUnit.Case
   doctest Transchange
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Transchange.TestRepo
+
+  test "new" do
+    assert Transchange.new == %Transchange{}
   end
+
+  test "pipe_ok with tuple" do
+    transchange =
+      Transchange.new
+      |> Transchange.pipe_ok({__MODULE__, :ok, []})
+
+    assert {__MODULE__, :ok, []} in transchange.ok_pipeline
+  end
+
+  test "pipe_ok with fun" do
+    fun = &(&1)
+
+    transchange =
+      Transchange.new
+      |> Transchange.pipe_ok(fun)
+
+    assert fun in transchange.ok_pipeline
+  end
+
+  test "pipe_error with tuple" do
+    transchange =
+      Transchange.new
+      |> Transchange.pipe_error({__MODULE__, :error, []})
+
+    assert {__MODULE__, :error, []} in transchange.error_pipeline
+  end
+
+  test "pipe_error with fun" do
+    fun = &(&1)
+
+    transchange =
+      Transchange.new
+      |> Transchange.pipe_error(fun)
+
+    assert fun in transchange.error_pipeline
+  end
+
+  test "update changeset" do
+    changeset = valid_changeset
+
+    Transchange.new
+    |> Transchange.update(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    assert_received {:update, %{changeset | action: :update}}
+    assert_received {:transaction, _}
+  end
+
+  test "insert changeset" do
+    changeset = valid_changeset
+
+    Transchange.new
+    |> Transchange.insert(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    assert_received {:insert, %{changeset | action: :insert}}
+    assert_received {:transaction, _}
+  end
+
+  test "delete changeset" do
+    changeset = valid_changeset
+
+    Transchange.new
+    |> Transchange.delete(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    assert_received {:delete, %{changeset | action: :delete}}
+    assert_received {:transaction, _}
+  end
+
+  test "update invalid changeset" do
+    changeset = invalid_changeset
+
+    Transchange.new
+    |> Transchange.update(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    refute_received {:update, ^changeset}
+    refute_received {:transaction, _}
+  end
+
+  test "insert invalid changeset" do
+    changeset = invalid_changeset
+
+    Transchange.new
+    |> Transchange.insert(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    refute_received {:insert, ^changeset}
+    refute_received {:transaction, _}
+  end
+
+  test "delete invalid changeset" do
+    changeset = invalid_changeset
+
+    Transchange.new
+    |> Transchange.delete(:changeset, changeset)
+    |> Transchange.run(TestRepo)
+
+    refute_received {:delete, ^changeset}
+    refute_received {:transaction, _}
+  end
+
+  test "run with no changesets" do
+    Transchange.new
+    |> Transchange.run(TestRepo)
+
+    refute_received {:transaction, _}
+  end
+
+  defp invalid_changeset, do: %Ecto.Changeset{valid?: false}
+  defp valid_changeset,   do: %Ecto.Changeset{valid?: true}
 end
